375a584ee83e6760a8035feb03cb76b3198a2089
f2cmb <f2cambourg@gmail.com>
2025-07-30 09:40:07 +0200

    cleanup

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -9,8 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Validator\Constraints as Assert;
-use App\Entity\Recipe;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class CategoryType extends AbstractType
 {
